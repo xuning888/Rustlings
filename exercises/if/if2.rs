9215d6027d3cf0f97e6499f2eafc53b8993da003
xuning <xningdd@163.com>
2025-01-25 01:15:03 +0800

    update

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -10,8 +10,12 @@
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
+    } else if fizzish == "literally anything" {
+        "baz"
     } else {
-        1
+        ""
     }
 }
 
